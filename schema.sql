615b94f167834bcb9e0d2b18f7abbfd79cc83905
Abdoul <abdoulramanediallo44@gmail.com>
2023-08-14 15:01:54 +0000

    create new db and tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
-);
+vet_clinic(# id serial PRIMARY KEY,
+vet_clinic(# name VARCHAR(255),
+vet_clinic(# date_of_birth DATE,
+vet_clinic(# escape_attempts INTEGER,
+vet_clinic(# neutered BOOLEAN,
+vet_clinic(# weight_kg DECIMAL(8, 2)
+vet_clinic(# );
